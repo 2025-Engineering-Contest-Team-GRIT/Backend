1dc96461c39dd8ef20f61b41c3b87d3394a8fa08
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-09-02 11:43:29 +0900

    Initial commit

---
+++ src/test/java/grit/guidance/GuidanceApplicationTests.java
@@ -0,0 +1,13 @@
+package grit.guidance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuidanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
