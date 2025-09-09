756b7c4003cf89a0cf8f8166baee26cf818ebea7
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-09-09 20:58:30 +0900

    feat: course 레포지토리 구현

---
+++ src/main/java/grit/guidance/domain/course/repository/TrackRepository.java
@@ -0,0 +1,12 @@
+package grit.guidance.domain.course.repository;
+
+import grit.guidance.domain.course.entity.Track;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TrackRepository extends JpaRepository<Track, Long> {
+    Optional<Track> findByTrackName(String trackName);
+}
