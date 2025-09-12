6e64643f96674c7af8767c7d71becfcdb5a708e4
최재현 <chlwogus180@naver.com>
2025-09-12 13:07:32 +0900

    WIP: GraduationPlan 수정 중

--- src/main/java/grit/guidance/domain/graduation/entity/GraduationPlan.java
+++ src/main/java/grit/guidance/domain/graduation/entity/GraduationPlan.java
@@ -24,19 +24,19 @@ public class GraduationPlan extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "graduation_plan_id")
-    private Long id;
+    private Long id; //졸업 계획 id를 통해 데이터베이스에 저장하고 불러오기가능
 
     @Column(name = "plan_name", nullable = false, length = 30)
-    private String planName;
+    private String planName; //졸업 계획 이름을 설정 ex) 26년 2월 ipp 포함 졸업계획
 
     // 다대일 관계 - graduation_plan과 user (단방향)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
-    private Users users;
+    private Users users;  //user_id를 외래키로 설정해서 user의 이수과목같은 정보 가져올 수 있음
 
     // 일대다 관계 - graduation_plan과 graduation_plan_course (양방향)
     @OneToMany(mappedBy = "graduationPlan", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<GraduationPlanCourse> graduationPlanCourses = new ArrayList<>();
+    private List<GraduationPlanCourse> graduationPlanCourses = new ArrayList<>(); //졸업계획과목의 id랑 외래키로 연결함
 
     @Builder
     private GraduationPlan(String planName, Users users) {
