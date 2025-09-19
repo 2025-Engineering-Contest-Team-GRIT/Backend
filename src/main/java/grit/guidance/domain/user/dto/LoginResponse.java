2b546f54b13fe5b3835efa7545686432bbfca510
Yeeyahou <your.email@example.com>
2025-09-20 06:41:37 +0900

    fix: 로그인, 크롤링 api 분리

--- src/main/java/grit/guidance/domain/user/dto/LoginResponse.java
+++ src/main/java/grit/guidance/domain/user/dto/LoginResponse.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record LoginResponse(
     @JsonProperty("status")
     Integer status,
+    @JsonProperty("is_new_user")
+    Boolean isNewUser,
     @JsonProperty("access_token")
     String accessToken
 ) {
