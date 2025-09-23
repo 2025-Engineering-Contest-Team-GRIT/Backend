0b9434e7ddcc5b0b2ea4bb9a4c7a4282a0bf8418
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-09-24 02:03:20 +0900

    엔티티스캔 설정

--- src/main/java/grit/guidance/GuidanceApplication.java
+++ src/main/java/grit/guidance/GuidanceApplication.java
@@ -2,10 +2,12 @@ package grit.guidance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EntityScan(basePackages = "grit.guidance.domain")
 public class GuidanceApplication {
 
 	public static void main(String[] args) {
